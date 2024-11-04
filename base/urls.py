81601c72dd95285e14d3aeabeb189dc3f97be406
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-11-04 10:52:11 +0500

    👤 feat: add user profile page

--- base/urls.py
+++ base/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
 
     path('', views.home, name='home'),
+    path('profile/<str:pk>/', views.user_profile, name='user_profile'),
     # Room CRUD
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.create_room, name='create_room'),
